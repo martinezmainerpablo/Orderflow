d28fe63a89afa3bef361d01b7f1478bd0497683b
Pablo Martínez <martinezmainerpablo@gmail.com>
2025-11-13 19:44:54 +0100

    orderflow
    Create and login User good

---
+++ Solution1/Orderflow.Identity/Controllers/AuthController.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Orderflow.Identity.DTOs;
+
+namespace Orderflow.Identity.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class AuthController : ControllerBase
+    {
+
+        private readonly UserManager<IdentityUser> _userManager;
+        private readonly SignInManager<IdentityUser> _signInManager;
+
+
+        public AuthController(UserManager<IdentityUser> userManager,
+                              SignInManager<IdentityUser> signInManager)
+        {
+            _userManager = userManager;
+            _signInManager = signInManager;
+        }
+
+        [HttpPost("login")]
+        public async Task<IActionResult> Login(LoginDto dto)
+        {
+            var user = await _userManager.FindByEmailAsync(dto.Email);
+            if (user == null) return Unauthorized();
+
+            var signInResult = await _signInManager.CheckPasswordSignInAsync(user, dto.Password, lockoutOnFailure: true);
+            if (!signInResult.Succeeded) return Unauthorized();
+
+            var roles = await _userManager.GetRolesAsync(user);
+
+            return Ok($"El usuario se ha logeado");
+        }
+
+    }
+}
---
+++ Solution1/Orderflow.Identity/Models/LoginDto.cs
@@ -0,0 +1,8 @@
+﻿namespace Orderflow.Identity.DTOs
+{
+    public class LoginDto
+    {
+        public string Email { get; set; } = "";
+        public string Password { get; set; } = "";
+    }
+}
